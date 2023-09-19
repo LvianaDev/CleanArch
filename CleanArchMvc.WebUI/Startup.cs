5949f10e755c44eaf7581275511c277258ad9d0d
lucasviana <lucasg.viana@outlook.com>
2023-09-19 03:47:45 -0300

    Adjustment of project version from .NET 7 to .NET 5

--- CleanArchMvc.WebUI/Startup.cs
+++ CleanArchMvc.WebUI/Startup.cs
@@ -1,24 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+﻿using CleanArchMvc.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using CleanArchMvc.Infra.Data.Context;
-using CleanArchMvc.Infra.IoC;
 
 namespace CleanArchMvc.WebUI
 {
 	public class Startup
 	{
-		public void ConfigureServices(IServiceCollection services) => services.AddDbContext<ApplicationDbContext>();
-
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
@@ -26,14 +12,11 @@ namespace CleanArchMvc.WebUI
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		//public void ConfigureServices(IServiceCollection services)
-		//{
-
-		//	services.AddInfraestruture(Configuration);
-		//	services.AddControllersWithViews();
-		//	services.AddRazorPages();
-		//}
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
